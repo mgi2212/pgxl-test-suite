b248d2dacd8d3cbffd2ec7bb3d4dc3b3a0c327eb
Dan Quigley <danq@quigleys.us>
2025-09-16 09:40:32 -0500

    Add pgxl-dev console script; ensure PGXL/FlexRadio exports

--- src/pgxl_testkit/__init__.py
+++ src/pgxl_testkit/__init__.py
@@ -1,2 +1,7 @@
 __all__=['__version__']
 __version__='0.2.0'
+# Re-export device classes for convenient import
+from .devices.pgxl import PGXL
+from .devices.flex import FlexRadio
+
+__all__ = ["PGXL", "FlexRadio"]
